257c9407f18da6b8455c74226e98ac223b293bbc
John Clarence Palma <johnclarencepalma7@gmail.com>
2026-04-25 20:34:44 +0800

    back, line affected new update

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+const contextSample = [
+  { id: 1, description: "Documents", quantity: 1, packed: false },
+  { id: 2, description: "Clothes", quantity: 2, packed: true }
+];
+
 
 export default function App() {
   return (
@@ -28,8 +33,20 @@ function UserForm() {
 function UserPackingList() {
   return (
     <div className="list">
-      Hello, Traveler!
+      <ul>
+        {contextSample.map((item) => <Item items={item} />)}
+      </ul>
     </div>
+  );
+}
+
+
+function Item({ items }) {
+  return (
+    <li>
+      <span>{items.quantity} {items.description}</span>
+      <button>❌</button>
+    </li>
   )
 }
 
@@ -37,7 +54,6 @@ function UserStatus() {
   return (
     <footer className="stats">
       <em>You have (items) item on your list, you already packed (items) (items%)</em>
-
     </footer>
   )
 }

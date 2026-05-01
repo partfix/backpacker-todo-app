6aa8a609c56418f67e8be1bb91789179dfb3cf2e
John Clarence Palma <johnclarencepalma7@gmail.com>
2026-05-01 10:56:19 +0800

    feat: add controlled element

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const contextSample = [
   { id: 1, description: "Documents", quantity: 1, packed: false },
   { id: 2, description: "Clothes", quantity: 2, packed: true }
@@ -21,21 +23,36 @@ function Header() {
 }
 
 function UserForm() {
+  // to store data from the form itself not the dom
+  const [description, setDescription] = useState('');
+  const [option, setOption] = useState(1);
 
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(e);
+
+    if (!description) return;
+
+    const newData = { description, option, packed: false, id: Date.now() }
+    console.log(newData);
+
+    setDescription('');
+    setOption(1);
+
   }
 
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <span>What are we bringing today?</span>
-      <select>
+      <select value={option} onChange={(e) => setOption(Number(e.target.value))}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map(num => <option value={num} key={num}>{num}</option>)}
         {/*this add empty array, and render it all by looping inside the option*/}
       </select>
-      <input type="text" name="text" placeholder="What's on your mind?"></input>
+      <input type="text" name="text"
+        placeholder="What's on your mind?"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}>
+      </input>
       <button>Add this</button>
     </form>
   );

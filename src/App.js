b14399bf8ff9bc8b6adc1aa51a8fd5e680498967
John Clarence Palma <johnclarencepalma7@gmail.com>
2026-06-13 23:15:11 +0800

    feat : add derive state (sortedInput, sortedDescription, sortedPacked)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import { useState } from "react";
+import { use, useState } from "react";
 
 
 export default function App() { // Grandparent 
-  const [items, setItems] = useState([])
 
-
+  const [items, setItems] = useState([]) //liftup state
 
 
   function handleItems(item) {
@@ -21,12 +20,19 @@ export default function App() { // Grandparent
     setItems((items) => items.map((item) => item.id === id ? { ...item, packed: !item.packed } : item)) // strike-through
   }
 
+  function clearList() {
+
+    const confirm = window.confirm("Are you sure you want to clear the list?")
+
+    if (confirm) setItems([])
+  }
+
 
   return (
     <div className="app">
       <Header />
       <UserForm onAdd={handleItems} /> {/*we can pass anything as a props including function*/}
-      <UserPackingList items={items} onDelete={handleDelete} onToogle={strike} />
+      <UserPackingList items={items} onDelete={handleDelete} onToogle={strike} onClear={clearList} />
       <UserStatus items={items} />
     </div>
   )
@@ -44,14 +50,12 @@ function UserForm({ onAdd }) {
   const [option, setOption] = useState(1);
   const [duration, setDuration] = useState(1);
 
-
-
   function handleSubmit(e) {
     e.preventDefault();
 
-    if (!description) return;
+    if (!description) return; // don't proceed
 
-    const newData = { id: Date.now(), description, option, duration, packed: false }
+    const newData = { id: Date.now(), description, option, duration, packed: false } //independent 
     console.log(newData);
     onAdd(newData)
 
@@ -77,20 +81,38 @@ function UserForm({ onAdd }) {
       </select>
 
       <span>duration </span>
+
       <select value={duration} onChange={(e) => setDuration(Number(e.target.value))}>
         {Array.from({ length: 31 }, (_, i) => i + 1).map(num => <option value={num} key={num}>{num}</option>)}
       </select>
       <button>Add this</button>
     </form>
+
   );
 }
 
-function UserPackingList({ items, onDelete, onToogle }) {
+function UserPackingList({ items, onDelete, onToogle, onClear }) {
+  const [sortby, setSortby] = useState('input')
+
+
+  let sorted; //derived state 
+
+  if (sortby === "input") sorted = items
+  if (sortby === "description") sorted = items.slice().sort((a, b) => a.description.localeCompare(b.description));
+  if (sortby === "packed") sorted = items.slice().sort((a, b) => Number(a.packed) - Number(b.packed))
   return (
     <div className="list">
       <ul>
-        {items.map((item) => <Item items={item} onDelete={onDelete} onToogleItem={onToogle} key={item.id} />)}
+        {sorted.map((item) => <Item items={item} onDelete={onDelete} onToogleItem={onToogle} key={item.id} />)}
       </ul>
+      <div className="actions">
+        <select value={sortby} onChange={(e) => setSortby(e.target.value)}>
+          <option value="input">Sort by Input</option>
+          <option value="description">Sort by Description</option>
+          <option value="packed">Sory by Packed Status</option>
+        </select>
+        <button onClick={onClear}>Clear list</button>
+      </div >
     </div>
   );
 }
@@ -103,15 +125,16 @@ function Item({ items, onDelete, onToogleItem }) {
         {items.option} {items.quantity} {items.description}
       </span>
       <button onClick={() => onDelete(items.id)}>❌</button>
-    </li>
+
+    </li >
   )
 }
 
 
 function UserStatus({ items }) {
-  if (!items.length) {
+  if (!items.length) { // derived state
     return (
-      <p className="stats"><em>Hey you goatta add something here, I'm kinda lonely!</em></p>
+      <p className="stats"><em>Are we goanna travel or what? Let's get going, come on!</em></p>
     )
   }
 
@@ -121,7 +144,7 @@ function UserStatus({ items }) {
 
   return (
     <footer className="stats">
-      <em>{percentage === 100 ? "you are ready to go!" : `You have ${numItems} item on your list, you already packed ${alreadyPacked} (${percentage}%)`}</em>
+      <em>{percentage === 100 ? "Enjoy your trip, you are ready to go!" : `You have ${numItems} item on your list, you already packed ${alreadyPacked} (${percentage}%)`}</em>
     </footer>
   )
 }
